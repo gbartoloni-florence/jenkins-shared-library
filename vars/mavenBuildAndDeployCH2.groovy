eee9be96995c4cdb28d7f337678b8729ea4efeac
Giacomo Bartoloni FCG <63246312+gbartoloni-florence@users.noreply.github.com>
2023-09-25 17:27:15 +0200

    Fix

--- vars/mavenBuildAndDeployCH2.groovy
+++ vars/mavenBuildAndDeployCH2.groovy
@@ -1,12 +1,13 @@
 def call(Map input) {
   pipeline {
-    agent {
-      docker {
-        image 'maven:3.8.6-jdk-8'
-      }
-    }
+    agent any
     stages {
-      stage('Checkout Repository') {
+      stage('Package') {
+        agent {
+          docker {
+            image 'maven:3.8.6-jdk-8'
+          }
+        }
         steps {
           script {
             sh 'mcn clean package'
