025878713032dbe2be0668ef7b15bfb913fff213
Giacomo Bartoloni FCG <63246312+gbartoloni-florence@users.noreply.github.com>
2023-09-26 12:37:56 +0200

    Added new pipeline

---
+++ deployMulesoftWithManifest.groovy
@@ -0,0 +1,19 @@
+def call(Map input) {
+  pipeline {
+    agent any
+    stages {
+      stage('Package') {
+        agent {
+          docker {
+            image 'maven:3.8.6-jdk-8'
+          }
+        }
+        steps {
+          script {
+            echo params.Application
+          }
+        }
+      }
+    }
+  }
+}
