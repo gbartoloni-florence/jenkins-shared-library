af12ed7aa023373a1e4e9636e387709f9f4d543d
Giacomo Bartoloni FCG <63246312+gbartoloni-florence@users.noreply.github.com>
2023-10-10 17:41:49 +0200

    Fix

--- vars/deployMulesoftWithManifest.groovy
+++ vars/deployMulesoftWithManifest.groovy
@@ -1,44 +1,50 @@
-@GrabResolver(name='mulesoft', root='https://repository-master.mulesoft.org/nexus/content/repositories/releases')
+@GrabResolver(name='mulesoft', root='https://repository.mulesoft.org/releases')
 @Grab(group='org.mule.tools.maven', module='mule-deployer', version='3.8.7')
 
 
 import org.yaml.snakeyaml.Yaml
 import org.mule.tools.model.anypoint.Cloudhub2Deployment
 
+
 def call(String environment) {
-  pipeline {
-    agent {
-      docker {
-        image 'node:18.14.0-alpine3.17'
-        reuseNode true
-      }
-    }
-    stages {
-      stage('Package') {
-        steps {
-          dir('manifests') {
-            git(
-              url: "https://github.com/gbartoloni-florence/mulesoft-manifests.git",
-              branch: "main",
-              changelog: true,
-              poll: false
-            )
-          }
-          script {
-            echo params.Application
-            echo environment
-            // sh "npm install -g anypoint-cli-v4"
-            // sh "anypoint-cli-v4 --version"
-
-            Yaml parser = new Yaml()
-            Map configuration = parser.load((new File(workspace + "/manifests/" + environment + "/" + params.Application + ".manifest.yaml")).text)
-            echo "config = $configuration"
-
-            Cloudhub2Deployment ch2deployment = new Cloudhub2Deployment()
-            ch2deployment.setvCores("0.1")
-          }
+    pipeline {
+        agent {
+            docker {
+                image 'node:18.14.0-alpine3.17'
+                reuseNode true
+            }
         }
-      }
+        stages {
+            stage('Package') {
+                steps {
+                    dir('manifests') {
+                        git(
+                                url: "https://github.com/gbartoloni-florence/mulesoft-manifests.git",
+                                branch: "main",
+                                changelog: true,
+                                poll: false
+                        )
+                    }
+                    script {
+                        echo params.Application
+                        echo environment
+                        // sh "npm install -g anypoint-cli-v4"
+                        // sh "anypoint-cli-v4 --version"
+
+                        Yaml parser = new Yaml()
+                        Map configuration = parser.load((new File(workspace + "/manifests/" + environment + "/" + params.Application + ".manifest.yaml")).text)
+                        echo "config = $configuration"
+
+                        Cloudhub2Deployment ch2deployment = new Cloudhub2Deployment()
+                        ch2deployment.setvCores("0.1")
+                        ch2deployment.setEnvironment("Dev")
+                        Cloudhub2Deployment ch2deployment = new Cloudhub2Deployment()
+                        ch2deployment.setApplicationName(params.Application + "-" + environment)
+
+                        echo ch2deployment.getApplicationName()
+                    }
+                }
+            }
+        }
     }
-  }
 }
