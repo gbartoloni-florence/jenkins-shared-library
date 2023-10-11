1455a9ae74797ec2df45a67ecae82d7b4fbef87f
Giacomo Bartoloni FCG <63246312+gbartoloni-florence@users.noreply.github.com>
2023-10-11 16:00:30 +0200

    WIP

--- src/it/clivet/cicd/sharedlibrary/utils/JenkinsLog.groovy
+++ src/it/clivet/cicd/sharedlibrary/utils/JenkinsLog.groovy
@@ -11,36 +11,36 @@ class JenkinsLog implements DeployerLog {
   private Logger log = Logger.getLogger('it.clivet.cicd.sharedlibrary.utils.JenkinsLog')
 
   public JenkinsLog() {
-    this.log = log;
+    this.log = log
   }
 
   @Override
   public void info(String charSequence) {
-    log.info(charSequence);
+    log.info(charSequence)
   }
 
   @Override
   public void error(String charSequence) {
-    log.error(charSequence);
+    log.error(charSequence)
   }
 
   @Override
   public void warn(String charSequence) {
-    log.warn(charSequence);
+    log.warn(charSequence)
   }
 
   @Override
   public void debug(String charSequence) {
-    log.info(charSequence);
+    log.info(charSequence)
   }
 
   @Override
   public void error(String charSequence, Throwable e) {
-    log.error(charSequence, e);
+    log.error(charSequence, e)
   }
 
   @Override
   public boolean isDebugEnabled() {
-    return log.isDebugEnabled();
+    return log.isDebugEnabled()
   }
 }
